e03a4937ceacebc3b0580d158c7200e1cf495fa3
Jetal <malijetal1234@gmail.com>
2021-01-23 02:59:58 +0530

    Add Navbar and Footer in Login Page

---
+++ client/src/components/footers/FooterSmall.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+
+class FooterSmall extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="FooterSmall">
+        <footer
+          className={
+            "absolute w-full bottom-0 bg-gray-900 pb-6"
+          }
+        >
+          <div className="container mx-auto px-4">
+            <div className="mb-6 border-b-1 border-gray-700">
+              <div className="flex flex-wrap items-center md:justify-between justify-center">
+                <div className="w-full md:w-4/12 px-4">
+                  <div className="text-sm text-gray-600 font-semibold py-1 text-center md:text-left">
+                    Copyright © {new Date().getFullYear()}{" "}
+                    <a href="..."
+                      className="text-white hover:text-gray-400 text-sm font-semibold py-1"
+                    >
+                      Creative Jetal
+                    </a>
+                  </div>
+                </div>
+                <div className="w-full md:w-8/12 px-4">
+                  <ul className="flex flex-wrap list-none md:justify-end justify-center">
+                    <li>
+                      <a
+                        href="https://www.creative-tim.com?ref=nr-footer-small"
+                        className="text-white hover:text-gray-400 text-sm font-semibold block py-1 px-3"
+                      >
+                        Creative Jetal
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://www.creative-tim.com/presentation?ref=nr-footer-small"
+                        className="text-white hover:text-gray-400 text-sm font-semibold block py-1 px-3"
+                      >
+                        About Us
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="http://blog.creative-tim.com?ref=nr-footer-small"
+                        className="text-white hover:text-gray-400 text-sm font-semibold block py-1 px-3"
+                      >
+                        Blog
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://github.com/creativetimofficial/notus-react/blob/master/LICENSE.md?ref=nr-footer-small"
+                        className="text-white hover:text-gray-400 text-sm font-semibold block py-1 px-3"
+                      >
+                        MIT License
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </div>
+    )
+  }
+}
+export default FooterSmall;
