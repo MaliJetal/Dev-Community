536f054e1f6d2c4e00930023600fa89d582d27cd
Jetal Mali <38008970+MaliJetal@users.noreply.github.com>
2021-07-14 01:43:00 +0530

    Update PagesDropdown.js

--- client/src/components/dropdowns/PagesDropdown.js
+++ client/src/components/dropdowns/PagesDropdown.js
@@ -21,7 +21,7 @@ const PagesDropdown = () => {
     <div className="PagesDropdown">
       <a
         href="#pablo"
-        className="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+        
         ref={btnDropdownRef}
         onClick={(e) => {
           e.preventDefault();
@@ -34,75 +34,61 @@ const PagesDropdown = () => {
         ref={popoverDropdownRef}
         className={
           (dropDownPopoverShow ? "block " : "hidden ") +
-          "bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48"
+          
         }
       >
         <span
-          className={"text-sm pt-2 pb-0 px-4 font-bold block w-full whitespace-no-wrap bg-transparent text-gray-500"}
+         
         >
           Admin
         </span>
         <Link
           to="/admin/dashboard"
           className={
-            "text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
+            
           }
         >
           Dashboard
         </Link>
         <Link
           to="/admin/dk"
-          className={
-            "text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
-          }
+          
         >
           DK
         </Link>
-        <div className="h-0 mx-4 my-2 border border-solid boder-gray-200"></div>
+        <div ></div>
         <span
-          className={
-            "text-sm pt-2 pb-0 px-4 font-bold block w-full whitespace-no-wrap bg-transparent text-gray-500"
-          }
+       
         >
           Auth Layout
         </span>
         <Link
           to="/auth/login"
-          className={
-            "text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
-          }
+          
         >
           Login
         </Link>
         <Link
           to="/auth/register"
-          className={
-            "text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
-          }
+          
         >
           Register
         </Link>
-        <div className="h-0 mx-4 my-2 border border-solid border-gray-200" />
+        <div  />
         <span
-          className={
-            "text-sm pt-2 pb-0 px-4 font-bold block w-full whitespace-no-wrap bg-transparent text-gray-500"
-          }
+          
         >
           No Layout
         </span>
         <Link
           to="/landing"
-          className={
-            "text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
-          }
+          
         >
           Landing
         </Link>
         <Link
           to="/profile"
-          className={
-            "text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
-          }
+          
         >
           Profile
         </Link>
