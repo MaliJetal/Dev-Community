a2abd2cc01497b7a3748c74306967b1598a9e32e
Jetal Mali <38008970+MaliJetal@users.noreply.github.com>
2021-07-14 01:46:06 +0530

    Update Footer.js

--- client/src/components/footers/Footer.js
+++ client/src/components/footers/Footer.js
@@ -3,13 +3,11 @@ import React from 'react';
 export default function Footer() {
   return (
     <div className="Footer">
-      <footer className="relative bg-gray-300 pt-8 pb-6">
+      <footer>
         <div
-          className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20 h-20"
-          style={{ transform: "translateZ(0)" }}
         >
           <svg
-            className="absolute bottom-0 overflow-hidden"
+            
             xmlns="http://www.w3.org/2000/svg"
             preserveAspectRatio="none"
             version="1.1"
@@ -18,61 +16,55 @@ export default function Footer() {
             y="0"
           >
             <polygon
-              className="text-gray-300 fill-current"
+             
               points="2560 0 2560 100 0 100"
             ></polygon>
           </svg>
         </div>
         <div className="container mx-auto px-4">
-          <div className="flex flex-wrap text-center lg:text-left">
-            <div className="w-full lg:w-6/12 px-4">
-              <h4 className="text-3xl font-semibold">Let's keep in touch!</h4>
-              <h5 className="text-lg mt-0 mb-2 text-gray-700">
+          <div >
+            <div>
+              <h4 >Let's keep in touch!</h4>
+              <h5 >
                 Find us on any of these platforms, we respond 1-2 business days.
               </h5>
-              <div className="mt-6 lg:mb-0 mb-6">
+              <div>
                 <button
-                  className="bg-white text-blue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
                   type="button"
                 >
                   <i className="fab fa-twitter"></i>
                 </button>
                 <button
-                  className="bg-white text-blue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
                   type="button"
                 >
                   <i className="fab fa-twitter"></i>
                 </button>
                 <button
-                  className="bg-white text-blue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
                   type="button"
                 >
-                  <i className="fab fa-facebook-square"></i>
+                  <i></i>
                 </button>
                 <button
-                  className="bg-white text-blue-900 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
                   type="button"
                 >
                   <i className="fab fa-linkedin"></i>
                 </button>
                 <button
-                  className="bg-white text-gray-900 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
-                  type="button"
+                 type="button"
                 >
                   <i className="fab fa-github"></i>
                 </button>
               </div>
             </div>
-            <div className="w-full lg:w-6/12 px-4">
-              <div className="flex flex-wrap items-top mb-6">
-                <div className="w-full lg:w-4/12 px-4 ml-auto">
-                  <span className="block uppercase text-gray-600 text-sm font-semibold mb-2">
+            <div>
+              <div>
+                <div>
+                  <span>
                     Useful Links
                   </span>
                   <ul className="list-unstyled">
                     <li>
                       <a
-                        className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
                         href="https://www.creative-tim.com/presentation?ref=nr-footer"
                       >
                         About Us
@@ -80,7 +72,6 @@ export default function Footer() {
                     </li>
                     <li>
                       <a
-                        className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
                         href="https://blog.creative-tim.com?ref=nr-footer"
                       >
                         Blog
@@ -88,7 +79,6 @@ export default function Footer() {
                     </li>
                     <li>
                       <a
-                        className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
                         href="https://github.com/MaliJetal/Dev-Community"
                       >
                         Github
@@ -96,39 +86,35 @@ export default function Footer() {
                     </li>
                   </ul>
                 </div>
-                <div className="w-full lg:w-4/12 px-4">
-                  <span className="block uppercase text-gray-600 text-sm font-semibold mb-2">
+                <div>
+                  <span>
                     Other Resources
                   </span>
                   <ul className="list-unstyled">
                     <li>
                       <a
-                        className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
-                        href="https://github.com/MaliJetal/Dev-Community"
+                       href="https://github.com/MaliJetal/Dev-Community"
                       >
                         MIT License
                       </a>
                     </li>
                     <li>
                       <a
-                        className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
-                        href="https://creative-tim.com/terms?ref=nr-footer"
+                       href="https://creative-tim.com/terms?ref=nr-footer"
                       >
                         Terms & Conditions
                       </a>
                     </li>
                     <li>
                       <a
-                        className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
-                        href="https://creative-tim.com/privacy?ref=nr-footer"
+                       href="https://creative-tim.com/privacy?ref=nr-footer"
                       >
                         Privacy Policy
                       </a>
                     </li>
                     <li>
                       <a
-                        className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
-                        href="https://creative-tim.com/contact-us?ref=nr-footer"
+                       href="https://creative-tim.com/contact-us?ref=nr-footer"
                       >
                         Contact Us
                       </a>
@@ -138,14 +124,14 @@ export default function Footer() {
               </div>
             </div>
           </div>
-          <hr className="my-6 border-gray-400" />
-          <div className="flex flex-wrap items-center md:justify-between justify-center">
-            <div className="w-full md:w-4/12 px-4 mx-auto text-center">
-              <div className="text-sm text-gray-600 font-semibold py-1">
+          <hr  />
+          <div>
+            <div >
+              <div >
                 Copyright © {new Date().getFullYear()} Dev-Community by{" "}
                 <a
                   href="https://www.creative-tim.com?ref=nr-footer"
-                  className="text-gray-600 hover:text-gray-900"
+                 
                 >
                   Dev-Community
                 </a>
